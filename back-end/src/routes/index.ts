4f6bac5731352ef660490e0d71d6677184714afa
Yves de Sá <yvesdsb98@gmail.com>
2021-11-12 23:49:33 -0300

    Creating records for users.

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import appointmentRouter from '../routes/appointments.routes';
+import usersRouter from '../routes/users.routes';
+
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
