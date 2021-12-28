901d509ede1cca666f5d96ea800a5057c29ee946
Yves de Sá <yvesdsb98@gmail.com>
2021-12-28 03:08:04 -0300

    Validating SignUp page with Yup.

--- front-end/src/pages/SignUp/index.tsx
+++ front-end/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
 import { Form } from "@unform/web";
 
@@ -7,11 +7,24 @@ import logoImg from '../../assets/logo.svg'
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { Container, Content, Background } from "./style";
+import * as Yup from 'yup';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string().required('E-mail é obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(3, 'No mínimo 3 dígitos')
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log({ error });
+    }
+  }, []);
 
   return (
     <Container>
