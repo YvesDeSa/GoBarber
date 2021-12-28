59857701eff9ae8fd4206db3a782f60a7df0bdc7
Yves de Sá <yvesdsb98@gmail.com>
2021-12-28 14:52:49 -0300

    Added cors in server.

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors'
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -11,12 +12,13 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.derectory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if(err instanceof AppError){
+  if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
