6d08a74bcf3126218b27b770649f99d7a5f3a7c2
Yves de Sá <yvesdsb98@gmail.com>
2022-02-08 15:07:38 -0300

    Updating user.

--- back-end/src/modules/users/services/CreateUserService.ts
+++ back-end/src/modules/users/services/CreateUserService.ts
@@ -4,13 +4,13 @@ import IHashProviders from '../providers/HashProviders/models/IHashProviders';
 
 import { inject, injectable } from 'tsyringe';
 
-interface Request {
+interface IRequest {
   name: string;
   email: string;
   password: string;
 }
 
-interface UserReturn {
+interface IUserReturn {
   password?: string;
 }
 
@@ -21,7 +21,7 @@ class CreateUserService {
     @inject('UsersRepository') private usersRepository: IUsersRepository,
     @inject('HashProvider') private hashProviders: IHashProviders) { }
 
-  public async execute({ name, email, password }: Request): Promise<UserReturn> {
+  public async execute({ name, email, password }: IRequest): Promise<IUserReturn> {
 
     const checkUserExists = await this.usersRepository.findByEmail(email);
 
